a581ba8b123c652943b0684c88ca60ac2e1158e9
fajar sidik priatna <jardik.oryza@gmail.com>
2024-07-24 23:45:45 +0700

    refactor toast and remove flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,24 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
-        'node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx,vue}',
-        'node_modules/flowbite/**/*.{js,jsx,ts,tsx}',
-    ],
+  content: [
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+    "./storage/framework/views/*.php",
+    "./resources/views/**/*.blade.php",
+    "./resources/js/**/*.vue",
+  ],
 
-    darkMode: 'class',
+  darkMode: "class",
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Quicksand', 'Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Quicksand", "Figtree", ...defaultTheme.fontFamily.sans],
+      },
     },
+  },
 
-    plugins: [forms, require('flowbite/plugin')],
+  plugins: [forms],
 };
