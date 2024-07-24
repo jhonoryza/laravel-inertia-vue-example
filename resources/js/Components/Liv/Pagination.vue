2c6ffe76888f419d167cafa9244c90dad03bb174
fajar sidik priatna <jardik.oryza@gmail.com>
2024-07-24 15:35:26 +0700

    fix layout pagination

--- resources/js/Components/Liv/Pagination.vue
+++ resources/js/Components/Liv/Pagination.vue
@@ -57,9 +57,9 @@ defineProps({
           preserve-state
           preserve-scroll
           :class="{
-            'flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white':
+            'flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white':
               nextPageUrl,
-            'flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg cursor-not-allowed dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:cursor-default':
+            'flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg cursor-not-allowed dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:cursor-default':
               !nextPageUrl,
           }"
         >
