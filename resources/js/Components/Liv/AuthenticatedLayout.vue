938a34def06d6a78165d60221bc6341961e8d236
fajar sidik priatna <jardik.oryza@gmail.com>
2024-07-25 01:46:14 +0700

    minor

--- resources/js/Components/Liv/AuthenticatedLayout.vue
+++ resources/js/Components/Liv/AuthenticatedLayout.vue
@@ -4,9 +4,9 @@ import ApplicationLogo from "@/Components/ApplicationLogo.vue";
 import Dropdown from "@/Components/Dropdown.vue";
 import DropdownLink from "@/Components/DropdownLink.vue";
 import { Link } from "@inertiajs/vue3";
-import ToggleDarkMode from "@/Components/Liv/ToggleDarkMode.vue";
-import AppToast from "@/Components/Liv/AppToast.vue";
-import SideBar from "@/Components/Liv/SideBar.vue";
+import ToggleDarkMode from "./ToggleDarkMode.vue";
+import AppToast from "./AppToast.vue";
+import SideBar from "./SideBar.vue";
 
 const showingNavigationDropdown = ref(false);
 </script>
